e629f7d7134d9c493481b2dba4fe3c7056117004
Huseyn <huseyn19.burhonov1999@gmail.com>
2021-03-17 19:33:49 +0500

    Second DZ

--- HW_1_16-03-21/Program.cs
+++ HW_1_16-03-21/Program.cs
@@ -7,16 +7,28 @@ namespace HW_1_16_03_21
     {
         static void Main(string[] args)
         {
+            //First Dz
             string myText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque corrupti sint accusantium." +
                             "Asperiores culpa totam assumenda dicta natus illo rem. Officia similique possimus nulla assumenda ad " +
-                            "iste odio quia dolor asperiores dolorum expedita id, eos libero eius minus beatae laborum ex repellat? ";
+                            "iste odio quia dolor asperiores dolorum expedita id, eos libero eius minus beatae laborum ex repellat?";
+
             string coding = string.Join("", myText.Select(t => (t == 'a') ? '1' : (t == 'e') ? '2' : (t == 'i') ? '3' : (t == 'o') ? '4' :
                                                                (t == 'u') ? '5' : (t == 'y') ? '6': t));
 
             string decoding = string.Join("", myText.Select(t => (t == '1') ? 'a' : (t == '2') ? 'e' : (t == '3') ? 'i' : (t == '4') ? 'o' :
-                                                               (t == '5') ? 'u' : (t == '6') ? 'y' : t));
+                                                                 (t == '5') ? 'u' : (t == '6') ? 'y' : t));
+            
+            //Second Dz
+            string dirtyText = "gdfgdf234dg54gf*23oP42";
+            int leftNumber = 0;
+            int rightNumber = 0;
+            double leftSide =  Convert.ToDouble(string.Join("", dirtyText.TakeWhile(t => t != '*' && t != '/' && t != '+' && t != '-').Where(t => int.TryParse(t.ToString(), out leftNumber)).ToArray()));
+            double rightSide = Convert.ToDouble(string.Join("", dirtyText.SkipWhile(t => t != '*' && t != '/' && t != '+' && t != '-').Where(t => int.TryParse(t.ToString(), out rightNumber)).ToArray()));
+
+          
             Console.WriteLine(coding);
             Console.WriteLine(decoding);
+            Console.WriteLine(leftSide * rightSide);
         }
     }
 }
