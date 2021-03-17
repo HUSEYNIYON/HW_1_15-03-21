b6c17e9f350dfdeac65b53d33b1cd05dce1cfbbd
Huseyn <huseyn19.burhonov1999@gmail.com>
2021-03-17 22:19:42 +0500

    Ooops! I forgot about switches

--- HW_1_16-03-21/Program.cs
+++ HW_1_16-03-21/Program.cs
@@ -9,7 +9,7 @@ namespace HW_1_16_03_21
         {
             //First Dz
             string myText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque corrupti sint accusantium." +
-                            "Asperiores culpa totam assumenda dicta natus illo rem. Officia similique possimus nulla assumenda ad " +
+                            "Asperiores culpa totam assumenda dicta natus illo rem. Officia similique possimus nulla assumenda ad" +
                             "iste odio quia dolor asperiores dolorum expedita id, eos libero eius minus beatae laborum ex repellat?";
 
             string coding = string.Join("", myText.Select(t => (t == 'a') ? '1' : (t == 'e') ? '2' : (t == 'i') ? '3' : (t == 'o') ? '4' :
@@ -24,6 +24,16 @@ namespace HW_1_16_03_21
             int rightNumber = 0;
             double leftSide =  Convert.ToDouble(string.Join("", dirtyText.TakeWhile(t => t != '*' && t != '/' && t != '+' && t != '-').Where(t => int.TryParse(t.ToString(), out leftNumber)).ToArray()));
             double rightSide = Convert.ToDouble(string.Join("", dirtyText.SkipWhile(t => t != '*' && t != '/' && t != '+' && t != '-').Where(t => int.TryParse(t.ToString(), out rightNumber)).ToArray()));
+            Console.Write("Choose your command:\n 1 --> *\n 2 --> /\n 3 --> +\n 4 --> - \n");
+            int operation = int.Parse(Console.ReadLine());
+                double result = operation switch
+                {
+                    1 => leftSide * rightSide,
+                    2 => leftSide / rightSide,
+                    3 => leftSide + rightSide,
+                    4 => leftSide - rightSide,
+                    _ => throw new ArgumentException("incorrect command!")
+                };
 
             //Third Dz
             string camelCase = "loremIpsumDolorSitAmetConsecteturAdipisicingElitNequeCorruptiSintAccusantium" +
@@ -34,8 +44,8 @@ namespace HW_1_16_03_21
             
             Console.WriteLine(coding);
             Console.WriteLine(decoding);
+            Console.WriteLine(result);
             Console.WriteLine(camelCaseText);
-            Console.WriteLine(leftSide * rightSide);
         }
     }
 }
